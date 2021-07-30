5f4298c160ed4a1feb17f18fa4c2f6ed31a9a918
ABISUKI <15310443abi@gmail.com>
2021-07-30 11:29:37 -0500

    SHOP1: clase para conexion a DB

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-__all__ = ["dep"]
+__all__ = ["dep", "deb"]
---
+++ lib/db.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import psycopg2
+from psycopg2 import pool
+from psycopg2.extensions import POLL_ERROR
+from psycopg2.extras import DictCursor
+from psycopg2.pool import SimpleConnectionPool
+
+
+class PostgresDB:
+    __pool = None
+
+    @classmethod
+    def initialize(cls, auth: dict) -> None:
+        cls.__pool = SimpleConnectionPool(minconn=2,
+                                          maxconn=5,
+                                          cursor_factory=DictCursor,
+                                          database=auth["db"],
+                                          user=auth["usr"],
+                                          password=auth["pwd"],
+                                          host=auth["host"],
+                                          port=auth["port"])
+    
+
+    @classmethod
+    def get_connection(cls) -> object:
+        return cls.__pool.getconn()
+
+
+    @classmethod
+    def return_connection(cls, connection: object) -> None:
+        PostgresDB.__pool.putconn(connection)
+
+
+    @classmethod
+    def close_all_connections(cls) -> None:
+        PostgresDB.__pool.closeall()
+
+
+class CursorPool():
+    """Get a cursor from a connection from pool of connections."""
+
+    def __init__(self, auth: dict):
+        self.connection = None
+        self._cursor = None
+
+
+    def __enter__(self) -> object:
+        # get a connection from the pool
+        self.connection = PostgresDB.get_connection()
+        self._cursor = self.connection.cursor()
+        return self._cursor
+    
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        # Rollback if not everything is good
+        if exc_val is not None:
+            
+            self.connection.rollback()
+        else:
+            self._cursor.close()
+            # commit the connection otherwise nothing is gonna write
+            self.connection.commit()
+        # put it back to pool
+        PostgresDB.return_connection(self.connection)
