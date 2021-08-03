9b5f989ec502848e94a9b3ad23e5c72c12762a6e
ABISUKI <15310443abi@gmail.com>
2021-08-03 18:57:24 -0500

    SHOP1: configuraciones

--- etc/__init__.py
+++ etc/__init__.py
@@ -1,9 +1,12 @@
+import os
 import yaml
 
 __all__ = ["settings"]
 
+DIR = f"{os.getcwd()}/etc/settings.yaml"
 SETTINGS = None
-with open("/home/nefta/Documentos/PROJECTS/cargamosShop/etc/settings.yaml", 'r') as stream:
+
+with open(DIR, 'r') as stream:
     try:
         SETTINGS = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
--- run.py
+++ run.py
@@ -5,7 +5,8 @@ from app.views import app
 
 host_name = socket.gethostname()
 host_ip = socket.gethostbyname(host_name)
-
+print(host_name)
+HOST = os.environ["FLASK_HOST"]
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host=HOST, port=8080, debug=True)
