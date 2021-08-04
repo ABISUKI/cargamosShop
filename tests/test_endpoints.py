ded2aeece95bf8e11dcf0e1bdab1b366bc336946
ABISUKI <15310443abi@gmail.com>
2021-08-03 20:31:34 -0500

    SHOP1: hotfix

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -6,16 +6,16 @@ import os
 import requests
 
 HOST = os.environ["FLASK_HOST"]
-
+PORT = os.environ["FLASK_PORT"]
 
 def test_get_shop():
-    response = requests.get(f"http://{HOST}/product/inventory/ELPHNP30LITEHWI")
+    response = requests.get(f"http://{HOST}:{PORT}/product/inventory/ELPHNP30LITEHWI")
     data = response.json()
     assert data["error"] == None
     assert response.status_code == 200
 
 def test_get_shops():
-    response = requests.get(f"http://{HOST}/shop/all")
+    response = requests.get(f"http://{HOST}:{PORT}/shop/all")
     data = response.json()
     assert data["error"] == None
     assert response.status_code == 200
